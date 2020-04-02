085418f6fef230d3f7faecee0953343040880985
georg <george.chen1338@gmail.com>
2020-04-02 15:31:50 -0400

    missing 2 tests

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,32 @@
+require 'pry'
 class Transfer
   # your code here
-  
+  attr_accessor :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  def valid?
+   # self.receiver.valid? && self.receiver.valid?
+   sender.valid? && receiver.valid? 
+  end
+  def execute_transaction
+    if self.sender.valid?
+      @sender.balance -= self.amount
+      @receiver.balance += self.amount
+      @status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += self.amount
+      @receiver.balance -= self.amount
+      @status = "reversed"
+    end
+  end
 end
